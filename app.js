98ae2f20a5eb44595e8ed467b5baaab3d66276d5
unclebconnor <bc.boxomail@gmail.com>
2017-10-17 12:43:36 -0700

    just cuz they told me to

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect("mongodb://localhost/mern-backend");
+mongoose.connect("mongodb://localhost/millertime");
 
 
 app.use('/', index);
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,18 +1,39 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var Job = require("../models/job");
+var User = require("../models/user");
 var router = express.Router();
 
 // router.use()
 router.get('/', function(req, res, next) {
-  // Job.create({
-  //   title: "Junior Dev",
-  //   company: "google",
-  //   location: "Kirkland"
-  // }, function(err, job){
-  //   if (err) return console.log(err);
-  //   res.send(job);
-  // });
+  User.create({
+    name: "Beavis",
+    email: "thegreatcornholio@teepee.bm",
+    brandingStatement: "Hey, Butt-head, this website kicks ass.",
+    unicornJob: "Whoa, unicorns are cool.  Heh heh."
+  }, function(err, job) {
+    Job.create({
+        title: "Junior Dev",
+        companyName: "Google",
+        skills: "HTML, CSS, JS, React, hipster vibes",
+        rating: "Unicorn",
+        statusString: "applied",
+
+    }, {
+        title: "Junior Dev",
+        companyName: "FaceBooks",
+        skills: "HTML, CSS, JS, React, hipster vibes",
+        rating: "Unicorn",
+        statusString: "applied",
+
+    }, function(err,job){
+        if (err) return console.log(err);
+        res.send(job);
+    })
+        
+  });
+
+
 
   // Job.find({}, function(err, jobs) {
   //   if (err) return console.log(err);
