91fb976e302310070819634a3efb414a0c64f3ff
willylikestokayak <wccoker@gmail.com>
2017-10-17 13:04:35 -0700

    reggie update

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,48 +1,16 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var Job = require("../models/job");
-var User = require("../models/user");
 var router = express.Router();
 
 // router.use()
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
     Job.create({
         title: "Junior Dev",
         company: "Google",
         skills: "HTML, CSS, JS, React, hipster vibes",
         rating: "Unicorn",
         statusString: "applied",
-=======
-  User.create({
-    name: "Beavis",
-    email: "thegreatcornholio@teepee.bm",
-    brandingStatement: "Hey, Butt-head, this website kicks ass.",
-    unicornJob: "Whoa, unicorns are cool.  Heh heh."
-  }, function(err, job) {
-    Job.create({
-        title: "Junior Dev",
-        companyName: "Google",
-        skills: "HTML, CSS, JS, React, hipster vibes",
-        rating: "Unicorn",
-        statusString: "applied",
-
-    }, {
-        title: "Junior Dev",
-        companyName: "FaceBooks",
-        skills: "HTML, CSS, JS, React, hipster vibes",
-        rating: "Unicorn",
-        statusString: "applied",
-
-    }, function(err,job){
-        if (err) return console.log(err);
-        res.send(job);
-    })
-
-  });
-
-
->>>>>>> 80899ae01e23d176f5bdec92b36d543edd20ce9d
 
     }, {
         title: "Junior Dev",
@@ -55,11 +23,12 @@ router.get('/', function(req, res, next) {
         if (err) return console.log(err);
         res.send(job);
     });
-
-    // Job.find({}, function(err, jobs) {
-    //   if (err) return console.log(err);
-    //   res.send(jobs);
-    // })
 });
+
+// Job.find({}, function(err, jobs) {
+//   if (err) return console.log(err);
+//   res.send(jobs);
+// })
+//});
 
 module.exports = router;
