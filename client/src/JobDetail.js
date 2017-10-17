e8229b586ad3669a02119fc28a4f7651ac322a6e
regbake <regbake@gmail.com>
2017-10-17 14:10:01 -0700

    committing minor changes to jobDetail to get current with master branch again

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
 class JobDetail extends Component {
-  constructor(props) 
+  constructor(props) {
+    super(props)
+    //sibling of jobList.js
+    this.state = {}
+
+  }
+
+
+
 }
