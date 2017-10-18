159f4d3f323565ea6b42ea0c0c13636f60bc44e7
unclebconnor <bc.boxomail@gmail.com>
2017-10-17 20:05:18 -0700

    some functioning routes and pushing some state to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import JobList from './JobList.js';
 import JobDetail from './JobDetail.js';
 import UserDetail from './UserDetail.js';
-//import Headline from './Headline.js';
+import Headline from './Headline.js';
 import './App.css';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      jobs:[],
       activeJob:''
     }
   }
+ 
   componentDidMount() {
-    fetch("/jobs")
-      .then(response => response.json())
-      .then(response => this.setState({jobs: response}))
+    fetch("/jobs/joblist")
+    .then((r) => r.json())
+    .then((r) => {
+      let arr = r;
+      this.setState({
+        jobs: arr
+      })
+    })
   }
+  
+
   render() {
+    console.log("APPSTATE", this.state)
     return (
       <Router>
         <div>
@@ -54,7 +65,8 @@ class App extends Component {
           <div id="wrapper">
             <div id="sidebar-wrapper" className="col-md-2">
                 <div id="sidebar">
-                    <JobList activeJob={this.state.activeJob}/>
+                    <Headline />
+                    <JobList activeJob={this.state.activeJob} jobs={this.state.jobs}/>
                 </div>
               </div>
               <div id="main-wrapper" className="col-md-10 pull-right">
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,36 +3,58 @@ var mongoose = require("mongoose");
 var Job = require("../models/job");
 var User = require("../models/user");
 var router = express.Router();
-// router.use()
-router.get('/', function(req, res, next) {
 
-  User.create({
-    name: "Beavis",
-    email: "thegreatcornholio@teepee.bm",
-    brandingStatement: "Hey, Butt-head, this website kicks ass.",
-    unicornJob: "Whoa, unicorns are cool.  Heh heh."
-  }, function(err, job) {
-    Job.create({
-        title: "Junior Dev",
-        companyName: "Google",
-        skills: "HTML, CSS, JS, React, hipster vibes",
-        rating: "Unicorn",
-        statusString: "applied",
-    }, {
-        title: "Junior Dev",
-        companyName: "FaceBooks",
-        skills: "HTML, CSS, JS, React, hipster vibes",
-        rating: "Unicorn",
-        statusString: "applied",
-    }, function(err,job){
-        if (err) return console.log(err);
-        res.send(job);
-    })
-        
+//DON'T DELETE...SEED DATA
+router.get('/', function(req, res, next) {
+//   User.create({
+//     name: "Beavis",
+//     email: "thegreatcornholio@teepee.bm",
+//     brandingStatement: "Hey, Butt-head, this website kicks ass.",
+//     unicornJob: "Whoa, unicorns are cool.  Heh heh."
+//   }, function(err, job) {
+//     Job.create({
+//         title: "Junior Dev",
+//         companyName: "Google",
+//         skills: "HTML, CSS, JS, React, hipster vibes",
+//         rating: "Unicorn",
+//         statusString: "applied",
+//     }, {
+//         title: "Junior Dev",
+//         companyName: "FaceBooks",
+//         skills: "HTML, CSS, JS, React, hipster vibes",
+//         rating: "Unicorn",
+//         statusString: "applied",
+//     }, function(err,job){
+//         if (err) return console.log(err);
+//         res.send(job);
+//     })      
   });
+
+//CREATE A NEW JOB.  USED BY NEW JOB BUTTON
+router.get('/addjob', function(req, res, next){
+  Job.create({
+    title:"New Job",
+    companyName: "New Company"
+  },function(err,job){
+    if(err) return console.log(err);
+    res.send(job);
+  })
+})
+
+//GET LIST OF JOBS
+router.get('/joblist',function(req,res,next){
+  Job.find({}, function(err,jobs){
+    if(err) return console.log(err);
+    res.send(jobs);
+  })
+
+
+})
+
+
   // Job.find({}, function(err, jobs) {
   //   if (err) return console.log(err);
   //   res.send(jobs);
   // })
-});
+// });
 module.exports = router;
